7b8e35b4869b44faf510da7fc74e847ba60bf503
Gavin Brown <gav@greycubes.net>
2021-01-16 16:15:06 +0200

    sqlite-phhammdist.c : added code to allow for either TEXT or INTEGER fields to be passed GNUmakefile: added debug make option

--- GNUmakefile
+++ GNUmakefile
@@ -10,10 +10,13 @@ endif
 CFLAGS+=	-fPIC
 LIBS+=		-lsqlite3
 
-all: sqlite-phhammdist.$(SUFFIX)
+all: sqlite-phhammdist.$(SUFFIX) sqlite-phhammdist_debug.$(SUFFIX)
 
 sqlite-phhammdist.$(SUFFIX): sqlite-phhammdist.c GNUmakefile
 	$(CC) -shared $(CFLAGS) -o $@ $< $(LIBS)
+
+sqlite-phhammdist_debug.$(SUFFIX): sqlite-phhammdist.c GNUmakefile
+	$(CC) -DDEBUG -shared $(CFLAGS) -o $@ $< $(LIBS)
 
 clean:
 	rm -f sqlite-phhammdist.$(SUFFIX)
--- sqlite-phhammdist.c
+++ sqlite-phhammdist.c
@@ -20,35 +20,59 @@ int ph_hamming_distance(const uint64_t hash1,const uint64_t hash2){
 static void phhammdist(sqlite3_context *context, int argc, sqlite3_value **argv){
    
     if (argc != 2) {
-        sqlite3_result_error(context, "phhammdist: requires 2 TEXT arguments", -1);
+        sqlite3_result_error(context, "phhammdist: requires 2 TEXT or 2 INTEGER arguments.", -1);
         return;
     }
 
     int datatype_arg1 = sqlite3_value_type(argv[0]);
     int datatype_arg2 = sqlite3_value_type(argv[1]);
     
-    //fprintf( stderr, "datatype_arg1: %d datatype_arg2:%d\n", datatype_arg1, datatype_arg2);
+    #ifdef DEBUG
+      fprintf( stderr, "datatype_arg1: %d datatype_arg2:%d\n", datatype_arg1, datatype_arg2);
+    #endif
 
-    if ((datatype_arg1 != SQLITE3_TEXT || datatype_arg2 != SQLITE3_TEXT)) {
-        sqlite3_result_error(context, "ph_hamming_distance: both arguments need to be of type STRING", -1);
+    int64_t INT_ph1 = 0;
+    int64_t INT_ph2 = 0;
+
+    uint64_t UINT_ph1 = 0;
+    uint64_t UINT_ph2 = 0;
+
+    if (datatype_arg1 == SQLITE3_TEXT && datatype_arg2 == SQLITE3_TEXT) {
+        const char *phBlob1 = sqlite3_value_blob(argv[0]);
+        const char *phBlob2 = sqlite3_value_blob(argv[1]);
+        char *ptr = NULL;
+
+        UINT_ph1 = strtoul(phBlob1, &ptr, 10);
+        UINT_ph2 = strtoul(phBlob2, &ptr, 10);
+    } else if (datatype_arg1 == SQLITE_INTEGER && datatype_arg2 == SQLITE_INTEGER) {
+        INT_ph1 = sqlite3_value_int64(argv[0]);
+        INT_ph2 = sqlite3_value_int64(argv[1]);
+
+        UINT_ph1 = (uint64_t) INT_ph1;
+        UINT_ph2 = (uint64_t) INT_ph2;
+        
+        #ifdef DEBUG
+          fprintf( stderr, "INT_ph1: %lld, INT_ph2: %lld\n", INT_ph1, INT_ph2);
+          fprintf( stderr, "ph1: %llu, ph2: %llu\n", UINT_ph1, UINT_ph2);
+        #endif
+        //ph1 = sqlite3_value_int64(argv[0]);
+        //ph2 = sqlite3_value_int64(argv[1]);
+    
+    } else {
+        sqlite3_result_error(context, "phhammdist: both arguments need to be of type TEXT or both arguments need to be of type INTEGER.", -1);
         return;
     }
+    #ifdef DEBUG
+      fprintf( stderr, "INT_ph1: %lld, INT_ph2: %lld\n", INT_ph1, INT_ph2);
+      fprintf( stderr, "UINT_ph1: %llu, UINT_ph2: %llu\n", UINT_ph1, UINT_ph2);    
+    #endif
 
-    const char *phBlob1 = sqlite3_value_blob(argv[0]);
-    const char *phBlob2 = sqlite3_value_blob(argv[1]);
-    char *ptr = NULL;
-
-    uint64_t ph1 = strtoul(phBlob1, &ptr, 10);
-    uint64_t ph2 = strtoul(phBlob2, &ptr, 10);
-    
-    //fprintf( stderr, "ph1: %llu, ph2: %llu\n", ph1, ph2);
-
-    uint64_t p_ham_distance = ph_hamming_distance(ph1,ph2);
+    uint64_t p_ham_distance = ph_hamming_distance(UINT_ph1,UINT_ph1);
     sqlite3_result_int(context, p_ham_distance);
 }
 
 int init_sqlite(sqlite3 *db){
-    return sqlite3_create_function(db, "phhammdist", -1, SQLITE_ANY, 0, phhammdist, 0, 0);
+    return sqlite3_create_function(db, "phhammdist", 2, SQLITE_ANY, 0, phhammdist, 0, 0);
 }
 
 #ifdef _WIN32
